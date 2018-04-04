52000498af2b5e33b413d104716d4eb68c8183b3
Shuichi Ohsawa <ohsawa0515@gmail.com>
2018-04-04 13:08:48 +0900

    enable multiple files

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 )
@@ -11,16 +12,24 @@ var n int
 func main() {
 	flag.IntVar(&n, "n", 10, "count")
 	flag.Parse()
-	file := flag.Args()[0]
-
-	f, err := os.OpenFile(file, os.O_RDONLY, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
+	files := flag.Args()
 	output := os.Stdout
-	if err := Head(f, n, output); err != nil {
-		log.Fatal(err)
+
+	for _, file := range files {
+		f, err := os.OpenFile(file, os.O_RDONLY, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		if len(files) > 1 {
+			fmt.Fprintf(output, "==> %s <==\n", file)
+		}
+		if err := Head(f, n, output); err != nil {
+			log.Fatal(err)
+		}
+		if len(files) > 1 {
+			fmt.Fprint(output, "\n")
+		}
 	}
 }
