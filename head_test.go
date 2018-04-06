1d394764b53b2293b74dc0ce61411fbf71567bda
Shuichi Ohsawa <ohsawa0515@gmail.com>
2018-04-06 17:52:31 +0900

    fix variables

--- head_test.go
+++ head_test.go
@@ -7,12 +7,12 @@ import (
 
 func TestHeadLine(t *testing.T) {
 	cases := []struct {
-		file   string
-		n      uint64
-		actual string
+		file     string
+		n        uint64
+		expected string
 	}{
-		{file: "A\nBBB\nC\nD\nF\n", n: 2, actual: "A\nBBB\n"},
-		{file: "A\nBBB\nCCCC\nDDDDD\nFFFFFF\n", n: 10, actual: "A\nBBB\nCCCC\nDDDDD\nFFFFFF\n"},
+		{file: "A\nBBB\nC\nD\nF\n", n: 2, expected: "A\nBBB\n"},
+		{file: "A\nBBB\nCCCC\nDDDDD\nFFFFFF\n", n: 10, expected: "A\nBBB\nCCCC\nDDDDD\nFFFFFF\n"},
 	}
 
 	for _, c := range cases {
@@ -22,9 +22,9 @@ func TestHeadLine(t *testing.T) {
 			t.Errorf("unextected error: %v", err)
 		}
 
-		expected := buf.Bytes()
-		actual := []byte(c.actual)
-		if bytes.Compare(expected, actual) != 0 {
+		actual := buf.Bytes()
+		expected := []byte(c.expected)
+		if bytes.Compare(actual, expected) != 0 {
 			t.Errorf("not matched; actual %v, expected %v", string(actual), string(expected))
 		}
 	}
@@ -32,14 +32,14 @@ func TestHeadLine(t *testing.T) {
 
 func TestHeadCharacter(t *testing.T) {
 	cases := []struct {
-		file   string
-		c      uint64
-		actual string
+		file     string
+		c        uint64
+		expected string
 	}{
-		{file: "A\nBBB\nC\nD\nF\n", c: 10, actual: "A\nBBB\nC\nD\n"},
-		{file: "A\nBBB\nCCCC\nDDDDD\nFFFFFF\n", c: 200, actual: "A\nBBB\nCCCC\nDDDDD\nFFFFFF\n"},
-		{file: "あいうえお\n一二三四五\n", c: 6, actual: "あい"},
-		{file: "あいうえお\n一二三四五\n", c: 22, actual: "あいうえお\n一二"},
+		{file: "A\nBBB\nC\nD\nF\n", c: 10, expected: "A\nBBB\nC\nD\n"},
+		{file: "A\nBBB\nCCCC\nDDDDD\nFFFFFF\n", c: 200, expected: "A\nBBB\nCCCC\nDDDDD\nFFFFFF\n"},
+		{file: "あいうえお\n一二三四五\n", c: 6, expected: "あい"},
+		{file: "あいうえお\n一二三四五\n", c: 22, expected: "あいうえお\n一二"},
 	}
 
 	for _, c := range cases {
@@ -49,9 +49,9 @@ func TestHeadCharacter(t *testing.T) {
 			t.Errorf("unextected error: %v", err)
 		}
 
-		expected := buf.Bytes()
-		actual := []byte(c.actual)
-		if bytes.Compare(expected, actual) != 0 {
+		actual := buf.Bytes()
+		expected := []byte(c.expected)
+		if bytes.Compare(actual, expected) != 0 {
 			t.Errorf("not matched; actual %v, expected %v", string(actual), string(expected))
 		}
 	}
