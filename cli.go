f5f95eac9ab074cb2088ccdc54b02ef8c627e8e1
Shuichi Ohsawa <ohsawa0515@gmail.com>
2018-04-06 17:31:47 +0900

    fixed  that `defer` is not included in the loop

--- cli.go
+++ cli.go
@@ -32,31 +32,45 @@ func (cli *CLI) Run(args []string) int {
 	}
 
 	files := flags.Args()
+	result := 0
+	showFileName := false
 	for _, file := range files {
-		f, err := os.OpenFile(file, os.O_RDONLY, 0)
-		if err != nil {
-			log.Fatal(err)
+		if len(files) > 1 {
+			showFileName = true
 		}
-		defer f.Close()
+		if result != cli.headFile(file, n, c, showFileName) {
+			return result
+		}
+	}
 
-		if len(files) > 1 {
-			fmt.Fprintf(cli.outStream, "==> %s <==\n", file)
+	return ExitCodeOK
+}
+
+func (cli *CLI) headFile(file string, n, c uint64, showFileName bool) int {
+	f, err := os.OpenFile(file, os.O_RDONLY, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	if showFileName {
+		fmt.Fprintf(cli.outStream, "==> %s <==\n", file)
+	}
+	// c オプションが指定された場合は N bytesまで表示する
+	// n オプションより優先する
+	if c > 0 {
+		if err := HeadCharacter(f, c, cli.outStream); err != nil {
+			log.Println(err)
+			return ExitError
 		}
-		// c オプションが指定された場合は N bytesまで表示する
-		if c > 0 {
-			if err := HeadCharacter(f, c, cli.outStream); err != nil {
-				log.Println(err)
-				return ExitError
-			}
-		} else {
-			if err := HeadLine(f, n, cli.outStream); err != nil {
-				log.Println(err)
-				return ExitError
-			}
+	} else {
+		if err := HeadLine(f, n, cli.outStream); err != nil {
+			log.Println(err)
+			return ExitError
 		}
-		if len(files) > 1 {
-			fmt.Fprint(cli.outStream, "\n")
-		}
+	}
+	if showFileName {
+		fmt.Fprint(cli.outStream, "\n")
 	}
 
 	return ExitCodeOK
